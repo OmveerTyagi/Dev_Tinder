5c3496eb5cca9a0204e95604f28ebafae4602432
Omveer Tyagi <omveertyagi544437@gmail.com>
2024-10-10 10:10:03 +0530

    Express server created

--- src/app.js
+++ src/app.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const app = express();
 
-
-
-app.use("/get",(req,res)=>{
-    res.send("Hello World!");
-})
-
-app.use("/",(req,res)=>{
+app.get("/",(req,res)=>{
     res.send("Hello From The Server");
 })
 
